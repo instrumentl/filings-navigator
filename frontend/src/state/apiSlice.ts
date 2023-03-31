aa8a5a417348698557f8ede04f88855ec4c36452
Brittany Hasty <brittany.r.hasty@gmail.com>
2023-03-31 10:58:40 -0700

    Add temp debug route for Heroku server debugging

--- frontend/src/state/apiSlice.ts
+++ frontend/src/state/apiSlice.ts
@@ -11,13 +11,19 @@ const backend = process.env.NODE_ENV === 'production' ?
   process.env.BACKEND_URL :
   'http://localhost:3000/api'
 
+async function logAndOutput (response: Response) {
+  const text = await response.text()
+  console.log(text)
+  return text
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: backend
   }) as BaseQueryFn<string | FetchArgs, unknown, ServerErrorType, {}>,
   endpoints: build => ({
     getAwards: build.query({
-      query: params => `/awards${params}`
+      query: params => ({url: `/awards${params}`, responseHandler: (response) => logAndOutput(response)})
     }),
     getAwardById: build.query({
       query: awardId => `/awards/${awardId}`
