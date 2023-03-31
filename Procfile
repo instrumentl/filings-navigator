880631c20c63b56aaffa672e70dba7fd7d49d6e8
Brittany Hasty <brittany.r.hasty@gmail.com>
2023-03-31 04:01:39 -0700

    Restructure project for Heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: bundle exec rails server -p $PORT -e $RAILS_ENV && cd frontend && npm install && npm run build && npm run start
--- frontend/src/components/awards/awardSearch.tsx
+++ frontend/src/components/awards/awardSearch.tsx
@@ -26,7 +26,6 @@ export default function AwardSearch() {
   }
   
   const displayResults = () => {
-    console.log(stateToParams())
     return (
       <div className="m-4">
         <hr />
